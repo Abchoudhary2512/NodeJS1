eed673d3b447bdb3646cd35a61dc4cd1125022b6
ajay2512 <bhakarajay033@gmail.com>
2024-04-04 00:38:44 +0530

    response and requests

--- HtmlFiles/app.js
+++ HtmlFiles/app.js
@@ -1,13 +1,13 @@
 const fs = require('fs')
 const http = require('http')
 //creating a server
-
+const html =  fs.readFileSync('./index.html','utf-8')
 const server = http.createServer((request,response)=>{
-    response.end('<h1>lskdfjlksd</h1>')
+    response.end(html)
 console.log("new request is received")
 });
 
 //starting a server
-server.listen(8000,'127.0.0.1',()=>{
+server.listen(8000,'127.0.0.2',()=>{
     console.log('server started')
 })
