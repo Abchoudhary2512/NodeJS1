c497817230da2f20341d07fb05ca1af3e65d6733
ajay2512 <bhakarajay033@gmail.com>
2024-04-07 22:35:01 +0530

    query string parsing

--- Data/app.js
+++ Data/app.js
@@ -1,25 +1,52 @@
 const fs = require('fs')
 const http = require('http')
+const url = require('url')
 //creating a server
 const html = fs.readFileSync('./index.html','utf-8')
 let products = JSON.parse(fs.readFileSync('./product.json','utf-8'))
+let productListHtml = fs.readFileSync('./product-list.html','utf-8')
+
+let productHtmlArray = products.map((prod)=>{
+    let output = productListHtml.replace('{{%IMAGE%}}',prod.productImage)
+     output  = output.replace('{{%IMAGE%}}',prod.name)
+     output = output.replace('{{%MODELNAME%}}', prod.modeName);
+         output = output.replace('{{%MODELNO%}}', prod.modelNumber);
+         output = output.replace('{{%SIZE%}}', prod.size);
+         output = output.replace('{{%CAMERA%}}', prod.camera);
+         output = output.replace('{{%PRICE%}}', prod.price);
+         output = output.replace('{{%COLOR%}}', prod.color);
+         output = output.replace('{{%ID%}}', prod.id);
+         output = output.replace('{{%ROM%}}', prod.ROM);
+         output = output.replace('{{%DESC%}}', prod.Description);
+
+         return output;
+;})
 const server = http.createServer((request,response)=>{
-   let path = request.url;
+   let {query, pathname: path} = url.parse(request.url,true);
+   //console.log(x);
+   //let path = request.url;
    
     if(path === '/' || path.toLocaleLowerCase() === '/home'){
         response.writeHead(200);
-        response.end(html);
+        response.end(html.replace('{{%CONTENT%}}','you are in home page'));
     }
     else if(path.toLocaleLowerCase() === '/about'){
         response.writeHead(200);
         response.end(html.replace('{{%CONTENT%}}',"You r in about page"));
     }
     else if(path.toLocaleLowerCase() === '/products'){
-        response.writeHead(200,{
-            'Content-Type':'application/json'
-        });
-        response.end("you are in products page")
-        console.log(products);
+        if(!query.id){
+            let productResponseHtml =   html.replace('{{%CONTENT%}}',productHtmlArray.join(','))
+            response.writeHead(200,{
+                'Content-Type':'text/html'
+            });
+            response.end(productResponseHtml)
+           console.log('success')
+        }
+        else{
+            response.end('this is product with id= ' + query.id)
+        }
+     
        
        
     }
